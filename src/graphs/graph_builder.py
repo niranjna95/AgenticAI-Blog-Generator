d3c9e1df9e26744a0fe6e0f4940f1d7e55853dcc
niranjna95 <niranjangiri51295@gmail.com>
2026-02-20 10:32:38 +0530

    Add langgraph configuration and initial graph builder implementation

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -28,3 +28,13 @@ class GraphBuilder:
         if usecase=="topic":
             self.build_topic_graph()
         return self.graph.compile()
+    
+
+
+
+## Below code is for the langsmit langgraph studio
+llm =GroqLLM().get_llm()
+## get the graph
+graph_builder = GraphBuilder(llm)
+graph = graph_builder.build_topic_graph().compile()
+
